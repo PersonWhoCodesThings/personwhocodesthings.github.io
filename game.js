3a753316f7f974c155b5d445f75e0cae9127a851
A Person Who Exists <erbrasel@madison.k12.wi.us>
2025-05-22 18:16:03 +0000

    game

--- game.js
+++ game.js
@@ -1,55 +1,48 @@
-var blcks = {
-    lc : 0, // left count
-    rc : 0, // right count
-    lh : 0, // left blocks height
-    rh : 0 // right blocks height
-}
-
-function start(level) {
-    // reset block counts
-    lc = 0
-    rc = 0
-    // set correct heights for each level
-    if (level == 1) {
-        blocks.lh = 1
-        blocks.rh = 1
-    }
-    if (level == 2) {
-        blocks.lh = 2
-        blocks.rh = 1
-    }
-    if (level == 3) {
-        blocks.lh = 3
-        blocks.rh = 2
-    }
-    if (level == 4) {
-        blocks.lh = 2
-        blocks.rh = 4
-    }
-    if (level == 5) {
-        blocks.lh = 4
-        blocks.rh = 3
-    }
-    if (level == 6) {
-        blocks.lh = 4
-        blocks.rh = 1
-    }
-    // create button objects and canvas
-    const lb = document.createElement() // left button
-    lb.className = "input"
-    lb.type = "button"
-    lb.width = "40px"
-    lb.height = "30px"
-    lb.class = "left-bttn"
-    const rb = document.createElement() // right button
-    rb.className = "input"
-    rb.type = "button"
-    rb.width = "40px"
-    rb.height = "30px"
-    rb.class = "right-bttn"
-    const c = document.createElement("c")
-    c.type = "canvas"
-    c.width = "400px"
-    c.height = "300px"
-    const ctx = c.getContext("2d")
-}
+const c = document.getElementById('c');
+const ctx = c.getContext('2d');
+const stats = {
+    speed : 10
+};
+var plyr = {
+    xpos : 0,
+    ypos : 0,
+    xmve : 0,
+    ymve : 0
+};
+var snk = {
+    xpos : 100,
+    ypos : 100,
+    xmve : 0,
+    ymve : 0
+};
+function tick() {
+    // player movement
+    player.xpos += player.xmve;
+    player.ypos += player.ymve;
+    // snake movement
+    if (Math.random() <= 0.1) { // 10% chance for snake to change direction
+        snk.xmve = 10(Math.random()-0.5);
+        snk.ymve = 10(Math.random()-0.5);
+    };
+    snk.xpos += snk.xmve;
+    snk.ypos += snk.ymve;
+    // Draw
+    ctx.fillStyle = "white"
+    ctx.fillRect(300, 300, 500, 500)
+    ctx.fillStyle = "red";
+    ctx.fillRect(plyr.xpos, plyr.ypos, 20, 20);
+    ctx.fillStyle = "green";
+    ctx.fillRect(snk.xpos, snk.ypos, 20, 20);
+};
+// Input
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'W') {
+        player.ymve -= stats.speed;
+    } else if (event.key === 'A') {
+        player.xmve -= stats.speed;
+    } else if (event.key === 'S') {
+        player.ymve += stats.speed;
+    } else if (event.key === 'D') {
+        player.xmve += stats.speed;
+    };
+});
